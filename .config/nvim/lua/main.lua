b0a6cfcddc7bd2fabf9637803b68f19dbacd737d
Kevin Chau <kkhaichau@gmail.com>
2021-12-31 14:37:32 -0800

    disble plugins

--- .config/nvim/lua/main.lua
+++ .config/nvim/lua/main.lua
@@ -1,3 +1,13 @@
+-- disable unused plugins
+local disabled_built_ins = {
+    "tutor",
+    "zipPlugin"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
+
 require("core.global")
 require("core.options").load_options()
 require("core.mappings").mappings()
