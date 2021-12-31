653e6a9d992f694c364f8f77bb00cc5f8c50c797
Kevin Chau <kkhaichau@gmail.com>
2021-12-31 13:23:26 -0800

    General styles

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+function M.load_options()
+    -- columns
+    vim.opt.cursorcolumn = true
+    vim.opt.number = true
+    vim.opt.signcolumn = "auto"
+    vim.opt.wrap = false
+
+    -- indent
+    vim.opt.expandtab = true
+    vim.opt.shiftwidth = 4
+    vim.opt.autoindent = true
+    vim.opt.smartindent = true
+
+    -- splits
+    vim.opt.splitbelow = true
+    vim.opt.splitright = true
+
+    -- usage
+    vim.opt.wildmenu = true
+    vim.opt.clipboard = "unnamedplus"
+
+    -- search
+    vim.opt.smartcase = true
+
+    -- optimize
+    vim.opt.lazyredraw = true
+
+    -- index folders for find command
+    vim.opt.path = "**/*"
+    vim.opt.wildignore:append(
+        {
+	    ".git/",
+	    "build",
+	    "coverage",
+	    "dist",
+        }
+    )
+
+end
+
+return M
