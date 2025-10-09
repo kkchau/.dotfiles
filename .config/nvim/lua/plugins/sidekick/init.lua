7ba1379e7b2e0b7323a3b09e57668b79bd8fff12
Kevin Chau <kevin.chau@color.com>
2025-10-08 22:46:32 -0700

    Fix NES mapping

--- .config/nvim/lua/plugins/sidekick/init.lua
+++ .config/nvim/lua/plugins/sidekick/init.lua
@@ -2,29 +2,25 @@ local M = {}
 
 function M.init()
     local sidekick = require("sidekick")
-    sidekick.setup({
-        keys = {
-            {
-                -- ctrl g
-                "<C-g>",
-                function()
-                    -- if there is a next edit, jump to it, otherwise apply it if any
-                    if not require("sidekick").nes_jump_or_apply() then
-                        return "<C-g>" -- fallback to normal tab
-                    end
-                    -- if you are using Neovim's native inline completions
-                    -- requires Neovim 0.12+
-                    --if vim.lsp.inline_completion.get() then
-                    --    return
-                    --end
-                    return "<C-g>"
-                end,
-                mode = { "i", "n" },
-                expr = true,
-                desc = "Goto/Apply Next Edit Suggestion",
-            }
-        }
-    })
+    sidekick.setup({})
+
+    vim.keymap.set(
+        "n",
+        "<C-g>",
+        function()
+            -- if there is a next edit, jump to it, otherwise apply it if any
+            if not sidekick.nes_jump_or_apply() then
+                return -- jumped or applied
+            end
+            -- if you are using Neovim's native inline completions
+            -- requires Neovim 0.12+
+            --if vim.lsp.inline_completion.get() then
+            --    return
+            --end
+            return "<C-g>"
+        end,
+        { noremap = true, silent = true, buffer = true, }
+    )
 end
 
 return M
