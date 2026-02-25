fb845f5866401da7f9fab791fbec8ed0f7bbdd0a
Kevin Chau <kevin.chau@color.com>
2026-02-24 16:16:25 -0800

    hard-code wait, diffview

--- .config/nvim/after/lsp/ruff.lua
+++ .config/nvim/after/lsp/ruff.lua
@@ -13,7 +13,17 @@ return {
         vim.api.nvim_create_autocmd("BufWritePre", {
             callback = function()
                 vim.lsp.buf.format()
-            end,
+
+                -- Run the fixAll codeaction
+                vim.lsp.buf.code_action({
+                    context = {
+                        diagnostics = {},
+                        only = { "source.fixAll" },
+                    },
+                    apply = true,
+                })
+                vim.wait(100)
+            end
         })
     end,
     filetypes = { "python" },
